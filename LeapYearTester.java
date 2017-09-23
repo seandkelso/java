df88f991770312e063e4dae3fe89f58126cd8f06
seandkelso <seankelso22@gmail.com>
2017-09-23 15:32:38 -0400

    Create LeapYearTester.java
    This program determines whether a given year is a leap year and uses the LeapYear constructor to do so.

---
+++ LeapYearTester.java
@@ -0,0 +1,17 @@
+/**
+ * This program determines whether a given year is a leap year and uses the LeapYear file to do so.
+ * 
+ * @author: <Sean Kelso>
+ * @date: <Sep 17, 2017>
+ */
+import java.util.Scanner;
+
+public class LeapYearTester
+{   
+public static void main(String[] args)
+   {
+    LeapYear firstLeapYear = new LeapYear();
+    System.out.println(firstLeapYear.isLeapYear());
+       
+   }
+}
