4d9b19db7cca999225d49bbfd5b382d651e63f39
seandkelso <seankelso22@gmail.com>
2017-09-24 18:57:29 -0400

    Create E62d.java

---
+++ E62d.java
@@ -0,0 +1,33 @@
+//****************************************
+//  E62d.java
+//  
+//  This class sums odd numbers between a and b, two user input values
+//
+//  Author: Sean Kelso
+//  Date: 9/23/17
+//*****************************************
+
+import java.util.Scanner;
+
+public class E62d
+{
+  public static void main(String[] args) 
+   { 
+   Scanner input = new Scanner(System.in);
+   System.out.print("What is your input for a? "); // asks user for input a
+      int a = input.nextInt(); //gets input a
+   System.out.print("What is your input for b? "); //asks user for input b
+      int b = input.nextInt(); //gets input b
+      
+   int sum = 0;
+   for (int i = a; i<= b; i++) //for loop from a through b, inclusive, adding by 1 each time   
+     {if (i%2 != 0) //modulus function to determine if number is odd -- ie. 3mod2 != 0, since 3/2 = 1, remainder 5.
+       {
+          sum = sum + i;      
+       } //closes if statement               
+           
+     } //closes for loop
+   System.out.println("The sum is " +sum);
+   } //closes main
+       
+} //closes E62d                                      
