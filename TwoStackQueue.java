8b59ae4baf9f9fb11faaf09a8bf6489072b9c7fb
seandkelso <seankelso22@gmail.com>
2018-05-18 09:58:20 -0500

    Create TwoStackQueue.java

---
+++ TwoStackQueue.java
@@ -0,0 +1,32 @@
+/*
+Sean Kelso
+* TwoStackQueue.java
+* Implements a Queue using two Stacks
+*/
+
+public class TwoStackQueue<T> {
+  private MyStack<T> inbox;
+  private MyStack<T> outbox;
+
+  public TwoStackQueue() {
+    inbox = new MyStack<>();
+    outbox = new MyStack<>();
+  }
+
+  // O(1)
+  public void enqueue(T item) {
+    inbox.push(item);
+  }
+
+  // O(1) when outbox is not empty
+  // O(n) when outbox is empty. n == inbox.size
+  public T dequeue() {
+    if (outbox.isEmpty()) {
+      while (!inbox.isEmpty()) {
+        outbox.push(inbox.pop());
+      }
+    }
+
+    return outbox.pop();
+  }
+}
